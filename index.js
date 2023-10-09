01f82bc931ce4160014d1bbb01fc8bba4c87c52c
mathzds <101145096+mathzds@users.noreply.github.com>
2023-10-08 21:19:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-const baseURL = "https://animes.vision/animes/kenpuu-denki-berserk?page=";
-const headers = {
-  "User-Agent":
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
-};
+const headers = { "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36" };
 
+const baseURL = "https://animes.vision/animes/bleach?page=";
 let pageNumber = 1;
+const allEpisodes = [];
 
 async function fetchPage(pageNumber) {
   const url = `${baseURL}${pageNumber}`;
@@ -20,14 +18,16 @@ async function fetchPage(pageNumber) {
       const itemLinks = [];
       $(".screen-item-thumbnail").each((index, element) => {
         const href = $(element).attr("href");
-        const title = $(element).siblings(".screen-item-info").find("h3.sii-title").text();
+        const title = $(element)
+          .siblings(".screen-item-info")
+          .find("h3.sii-title")
+          .text();
         const imgSrc = $(element).find("img.sit-img").attr("src");
-      
+
         if (href && href.includes("episodio")) {
           itemLinks.push({ href, title, imgSrc });
         }
       });
-      
 
       if (itemLinks.length === 0) {
         console.log(`Página ${pageNumber}: vazia`);
@@ -35,14 +35,15 @@ async function fetchPage(pageNumber) {
       } else {
         console.log(`Página ${pageNumber}:`);
         console.log(itemLinks);
+        allEpisodes.push(...itemLinks);
         return false;
       }
     } else {
-      console.error(`Erro na página ${pageNumber}: ${response.status}`);
+      console.error(`${pageNumber}: ${response.status}`);
       return false;
     }
   } catch (error) {
-    console.error(`Erro na página ${pageNumber}: ${error}`);
+    console.error(`${pageNumber}: ${error}`);
     return false;
   }
 }
@@ -51,9 +52,13 @@ async function fetchPage(pageNumber) {
   while (true) {
     const isPageEmpty = await fetchPage(pageNumber);
     if (isPageEmpty) {
-      console.log("Parando de listar devido a uma página vazia.");
+      console.log("Página vazia.");
       break;
     }
     pageNumber++;
   }
+
+  const jsonEpisodes = JSON.stringify(allEpisodes, null, 2);
+  console.log("JSON:");
+  console.log(jsonEpisodes);
 })();
